25aec1d192e5f633967d9bf6dd2a319227eaa901
aa10 <aa10@192.168.5.160>
2020-06-02 13:44:33 +0900

    A-9(13:44)

--- kikkawa_Test2/src/kikkawa_Test/Test002.java
+++ kikkawa_Test2/src/kikkawa_Test/Test002.java
@@ -6,6 +6,8 @@ public class Test002 {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("123456789");
 		System.out.println(123456789);
+		System.out.println("123456789");
+		System.out.println(123456789);
 	}
 
 }
