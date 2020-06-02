2e212709f9e63e83a7fd8246cc9505a5e7e1da80
aa10 <aa10@DESKTOP-D8Q6VMT>
2020-06-02 12:20:24 +0900

    B-6(12:19)Aが別ファイルをUPしているとしても、バージョンが異なるので、競合するというパターン。

--- kikkawa_Test/src/kikkawa_Test/Test001.java
+++ kikkawa_Test/src/kikkawa_Test/Test001.java
@@ -4,9 +4,7 @@ public class Test001 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println(123456789);
-		System.out.println("kakikukeko");
-		System.out.println("aiueo");
+		System.out.println(999999999);
 	}
 
 }
