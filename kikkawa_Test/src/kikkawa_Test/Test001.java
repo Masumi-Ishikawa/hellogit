cf03c277e2562185859bea824c55c4aa6ef80d8b
aa10 <aa10@DESKTOP-D8Q6VMT>
2020-06-02 19:13:40 +0900

    B-8(19:13)

--- kikkawa_Test/src/kikkawa_Test/Test001.java
+++ kikkawa_Test/src/kikkawa_Test/Test001.java
@@ -6,10 +6,6 @@ public class Test001 {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println(999999999);
 		System.out.println(123456789);
-		System.out.println("kakikukeko");
-		System.out.println("aiueo");
-		System.out.println("kakikukeko");
-		System.out.println("aiueo");
 	}
 
 }
