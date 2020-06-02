08cedc7156d17232e4832dab1afac546e2858989
aa10 <aa10@192.168.5.160>
2020-06-02 20:42:50 +0900

    A-11(20:42)

---
+++ kikkawa_Test3/src/kikkawa_Test/Test002.java
@@ -0,0 +1,11 @@
+package kikkawa_Test;
+
+public class Test002 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("marumaru");
+		System.out.println("marumaru");
+	}
+
+}
