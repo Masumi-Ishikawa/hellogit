6d4dc3393f5e8018de51be844e5904b252cfd5ee
aa10 <aa10@DESKTOP-D8Q6VMT>
2020-06-03 10:56:37 +0900

    B10:56

--- kikkawa_Test/src/kikkawa_Test/Test002.java
+++ kikkawa_Test/src/kikkawa_Test/Test002.java
@@ -4,8 +4,6 @@ public class Test002 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println(999999999);
-		System.out.println("fsdhalshdj");
 		System.out.println("fsdhalshdj");
 	}
 
