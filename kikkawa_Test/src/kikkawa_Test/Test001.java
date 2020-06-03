47baa439b6b0e00aef2d250747dd5e0fd182d9c9
aa10 <aa10@192.168.5.160>
2020-06-03 10:55:53 +0900

    A(10:55)

--- kikkawa_Test/src/kikkawa_Test/Test001.java
+++ kikkawa_Test/src/kikkawa_Test/Test001.java
@@ -5,8 +5,6 @@ public class Test001 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println(999999999);
-		System.out.println("fsdhalshdj");
-		System.out.println("fsdhalshdj");
 	}
 
 }
