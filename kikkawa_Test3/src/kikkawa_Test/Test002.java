8330b6d42897a7538c2f1c1a7c2fe2b86432fe31
aa10 <aa10@DESKTOP-D8Q6VMT>
2020-06-03 09:22:27 +0900

    B-5(9:22)

--- kikkawa_Test3/src/kikkawa_Test/Test002.java
+++ kikkawa_Test3/src/kikkawa_Test/Test002.java
@@ -7,6 +7,8 @@ public class Test002 {
 		System.out.println("marumaru");
 		System.out.println("marumaru");
 		System.out.println("marumaru");
+		System.out.println("marumaru");
+		System.out.println("marumaru");
 	}
 
 }
