83eadba8e5ba1891966b4dc124c7dbba4fee349c
aa10 <aa10@192.168.5.160>
2020-06-11 09:16:58 +0900

    HP0916

--- kikkawa_Test/src/kikkawa_Test/Test001.java
+++ kikkawa_Test/src/kikkawa_Test/Test001.java
@@ -5,6 +5,8 @@ public class Test001 {
 	public static void main(String[] args) {
 		System.out.println(999999999);
 		System.out.println(999999999);
+		System.out.println(999999999);
+		System.out.println(999999999);
 	}
 
 }
