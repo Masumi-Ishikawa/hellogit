a9ffddf542103b6c41ec9d62b4b1eace2ff6066b
aa10 <aa10@192.168.5.160>
2020-06-11 12:08:12 +0900

    HP1207

--- kikkawa_Test/src/test/Test001.java
+++ kikkawa_Test/src/test/Test001.java
@@ -3,9 +3,6 @@ package test;
 public class Test001 {
 
 	public static void main(String[] args) {
-		System.out.println(999999999);
-		System.out.println(999999999);
-		System.out.println(999999999);
-		System.out.println(999999999);
+		System.out.println(123456789);
 	}
 }
