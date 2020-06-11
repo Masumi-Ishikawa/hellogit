1615a9642a7b4372c5941d2c0a3ad2d4c9cbf23b
aa10 <aa10@192.168.5.160>
2020-06-11 13:29:17 +0900

    HP1329

---
+++ kikkawa_Test/src/test/Test004.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test004 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("fsdhalshdj");
+
+		System.out.println("fsdhalshdj");
+	}
+
+}
