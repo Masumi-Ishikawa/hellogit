f06e101e58697ce42b0416a253c9e26e00117944
aa10 <aa10@192.168.5.160>
2020-06-11 13:38:59 +0900

    HP1338

--- kikkawa_Test/src/test/Test002.java
+++ kikkawa_Test/src/test/Test002.java
@@ -5,6 +5,8 @@ public class Test002 {
 	public static void main(String[] args) {
 		System.out.println("fsdhalshdj");
 		System.out.println("fsdhalshdj");
+		System.out.println("fsdhalshdj");
+		System.out.println("fsdhalshdj");
 	}
 
 }
